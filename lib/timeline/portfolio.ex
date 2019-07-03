041479287fad652c24674f27715027d9168549c0
Kostiantyn Manakov <monyashadow@gmail.com>
2019-07-03 19:50:53 +0300

    Timeline portfolio calculations

--- lib/timeline/portfolio.ex
+++ lib/timeline/portfolio.ex
@@ -30,10 +30,58 @@ defmodule Timeline.Portfolio do
       {:err, errors}
     else
       res = res
-      |> Map.new(fn {task, {:ok, {symbol, {:ok, response}}}} ->
-        {symbol, response.body["history"]}
+      # map all correct responses to histories with each entry marked with symbol
+      |> Enum.map(fn {task, {:ok, {symbol, {:ok, response}}}} ->
+        response.body["history"]
+        |> Enum.map(fn it -> {symbol, it} end)
+      end)
+      # zip and map from tuples to {date, %{symbol => value}}
+      |> Enum.zip
+      |> Enum.map(fn tuple ->
+        {_symbol, {date, _}} = elem(tuple, 1)
+        values = tuple
+        |> Tuple.to_list
+        |> Map.new (fn
+          {symbol, {_date, %{"close" => close}}} -> {symbol, String.to_float(close)}
+        end)
+        {date, values}
       end)
       {:ok, res}
+    end
+  end
+
+  def allocate_portfolio(amount, percentages, values) do
+    Map.merge(percentages, values, fn _k, percentage, value ->
+      Float.floor(amount * percentage / (100 * value), 2)
+    end)
+  end
+
+  def calculate_portfolio_value(portfolio, values) do
+    Map.merge(portfolio, values, fn _k, stocks, value ->
+      Float.floor(stocks*value, 2)
+    end)
+    |> Enum.reduce(0, fn {_k, val}, acc -> acc + val end)
+  end
+
+  def calculate_portfolio_values(amount, percentages, date) do
+    case get_data(Map.keys(percentages), date) do
+      {:ok, data} ->
+        {_date, values} = hd(data)
+        initial_portfolio = allocate_portfolio(amount, percentages, values)
+
+        {portfolio_values, final_portfolio} = data
+        |> Enum.map_reduce(initial_portfolio, fn {date, values}, portfolio ->
+          data_point = %{
+            date: date,
+            value: calculate_portfolio_value(portfolio, values),
+            values: values,
+            portfolio: portfolio
+          }
+          {data_point, portfolio}
+         end)
+        {:ok, portfolio_values, final_portfolio}
+      {:err, errors} ->
+        {:err, errors}
     end
   end
 end
--- test/timeline/portfolio_test.exs
+++ test/timeline/portfolio_test.exs
@@ -2,13 +2,39 @@ defmodule Timeline.PortfolioTest do
   use ExUnit.Case, async: true
   import Timeline.Portfolio
 
-  test "get data" do
-    {:ok, data} = get_data(["AAPL", "GOOGL", "VTI"])
-    assert %{"GOOGL" => google_data, "AAPL" => apple_data, "VTI" => vti_data} = data
+  # test "get data" do
+  #   {:ok, data} = get_data(["AAPL", "GOOGL", "VTI"], ~D(2005-10-10))
+  #   assert {~D[2005-10-10], %{"AAPL" => _, "GOOGL" => _, "VTI" => _}} = hd(data)
+  # end
+
+  # test "get data with errors" do
+  #   {:err, errors} = get_data(["AAPL", "oops", "wrong"])
+  #   assert Enum.count(errors) == 2
+  # end
+
+  test "allocate portfolio" do
+    portfolio = allocate_portfolio(1000,
+      %{"AAPL" => 50, "GOOGL" => 25, "VTI" => 25}, %{"AAPL" => 1, "GOOGL" => 1, "VTI" => 1})
+    assert %{"AAPL" => 500.0, "GOOGL" => 250.0, "VTI" => 250.0} = portfolio
   end
 
-  test "get data with errors" do
-    {:err, errors} = get_data(["AAPL", "oops", "wrong"])
-    assert Enum.count(errors) == 2
+  test "calculate portfolio value" do
+    portfolio = allocate_portfolio(1000,
+      %{"AAPL" => 50, "GOOGL" => 25, "VTI" => 25}, %{"AAPL" => 1, "GOOGL" => 1, "VTI" => 1})
+    portfolio_value = calculate_portfolio_value(portfolio, %{"AAPL" => 2, "GOOGL" => 2, "VTI" => 2})
+    assert portfolio_value == 2000
+  end
+
+  test "calculate portfolio values" do
+    {:ok, portfolio_values, _} = calculate_portfolio_values(1000,
+      %{"AAPL" => 50, "GOOGL" => 25, "VTI" => 25}, ~D[2005-10-10])
+    IO.inspect portfolio_values
+    assert %{date: ~D[2005-10-10], value: _, portfolio: _, values: _} = hd(portfolio_values)
+  end
+  test "calculate portfolio values with errors" do
+    {:err, errors} = calculate_portfolio_values(1000,
+      %{"AAPL" => 50, "GOOGLSdas1321" => 25, "VTI" => 25}, ~D[2005-10-10])
+
+    assert Enum.count(errors) == 1
   end
  end
